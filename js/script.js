e0329009799fccf8b2d681266aa93280843e3da4
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-04-13 19:49:25 +0200

    header done landing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-console.log("Script loaded");
+function setToast(message = "") {
+	document.querySelector("#toaster").textContent = message;
+	document.querySelector("#toaster").className = "show";
+
+	setTimeout(() => {
+		document.querySelector("#toaster").className = "hide";
+		document.querySelector("#toaster").textContent = "";
+	}, 3000);
+}
+
+function addListenerToCopyButton() {
+	const emailAddress = "joachim.jasmin@gmail.com";
+
+	document.querySelector("#copy-button").addEventListener("click", function () {
+		navigator.clipboard.writeText(emailAddress);
+
+		this.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon"">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
+    </svg>
+    `;
+
+		setToast("Email copié !");
+
+		setTimeout(() => {
+			document.querySelector("#copy-button").innerHTML = `
+      <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+        />
+      </svg>
+      `;
+		}, 2500);
+	});
+}
+
+/* ---------------------------------------------------------------- */
+/*                              ON LOAD                             */
+/* ---------------------------------------------------------------- */
+
+addListenerToCopyButton();
